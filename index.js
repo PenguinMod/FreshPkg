02ea178a4550b4d12d3622aed8fd536b96adcce6
Fresh <93781766+FreshPenguin112@users.noreply.github.com>
2023-01-23 09:55:55 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,1 @@
-alert('{pkgname}')
+function httpGet(e){var t=new XMLHttpRequest;return t.open("GET",e,!1),t.send(null),t.responseText};vm.addSprite(new Uint8Array(httpGet("https://github.com/FreshPenguin112/FreshPkg/blob/main/pkgs/{pkgname}/{pkgname}.sprite3?raw=true");))
